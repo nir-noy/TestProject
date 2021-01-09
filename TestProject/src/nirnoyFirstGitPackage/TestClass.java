6d9f9fb57ceb84c2ad5414574810ba156836ab49
nir-noy <nirnoydmayhem3@gmail.com>
2021-01-09 16:09:49 +0530

    Created package and updated class

---
+++ TestProject/src/nirnoyFirstGitPackage/TestClass.java
@@ -0,0 +1,10 @@
+package nirnoyFirstGitPackage;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Welcome");
+	}
+
+}
